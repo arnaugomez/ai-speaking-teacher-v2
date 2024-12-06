bdc591c89bcdecbdf085618161f158350bf233ef
Arnau Gómez <agomez1f@gmail.com>
2024-12-06 21:11:51 +0100

    feat: initial form ui

---
+++ src/common/ui/styles/text-styles.ts
@@ -0,0 +1,15 @@
+export const textStyles = {
+  logo: "text-2xl font-bold",
+  h1: "text-4xl font-extrabold tracking-tight lg:text-5xl",
+  h2: "text-3xl font-semibold tracking-tight",
+  h3: "text-2xl font-semibold tracking-tight",
+  h4: "text-xl font-semibold tracking-tight",
+  p: "text-base font-normal leading-6",
+  base: "text-base font-normal",
+  lead: "text-xl text-muted-foreground",
+  large: "text-lg font-semibold",
+  small: "text-sm font-medium leading-none",
+  tableLink: "text-sm font-medium leading-none hover:underline",
+  muted: "text-sm text-muted-foreground",
+  mutedLink: "text-sm text-muted-foreground hover:text-slate-700",
+};
--- src/teacher/ui/pages/teacher-page.tsx
+++ src/teacher/ui/pages/teacher-page.tsx
@@ -1,4 +1,8 @@
+import { Button } from "~/common/ui/shadcn/components/ui/button";
 import type { Route } from "./+types/teacher-page";
+import { Textarea } from "~/common/ui/shadcn/components/ui/textarea";
+import { cn } from "~/common/ui/shadcn/lib/utils";
+import { textStyles } from "~/common/ui/styles/text-styles";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,5 +12,23 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <>Teacher page</>;
+  return (
+    <div className="px-4 py-16">
+      <div className="max-w-prose mx-auto">
+        <TeacherForm />
+      </div>
+    </div>
+  );
+}
+
+function TeacherForm() {
+  return (
+    <form>
+      <h1 className={cn(textStyles.h1)}>AI speaking lessons</h1>
+      <div className="h-6"></div>
+      <Textarea placeholder="Enter the content or topic of the lesson" />
+      <div className="h-4"></div>
+      <Button type="submit">Submit</Button>
+    </form>
+  );
 }
